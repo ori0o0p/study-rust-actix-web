c6b03567117f5d56fab06ffaec12e91f9b1bcfd1
101 <tmddnjs001133@dsm.hs.kr>
2024-04-28 20:19:10 +0900

    update :: main.rs
    add api

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+mod service;
+mod model;
+
 use actix_cors::Cors;
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+use actix_web::{App, get, HttpResponse, HttpServer, post, Responder, web};
+use crate::model::UploadArticleDto;
+
+#[get("/api/{id}")]
+async fn get_article(id: web::Path<u32>) -> HttpResponse {
+    HttpResponse::Ok()
+        .json(service::get_article_default(id.clone()).await)
+}
 
 #[get("/api")]
-async fn hello() -> impl Responder {
+async fn find_all() -> HttpResponse {
     HttpResponse::Ok()
-        .content_type("text/plain; charset=utf-8")
-        .body("아일릿")
+        .json(service::find_all().await)
+}
+
+#[post("/api")]
+async fn upload_article(upload_article_dto: web::Json<UploadArticleDto>) -> HttpResponse {
+    service::upload_article(upload_article_dto.into_inner()).await;
+
+    HttpResponse::Created()
+        .body(())
 }
 
 #[actix_web::main]
@@ -20,7 +37,9 @@ async fn main() -> std::io::Result<()> {
                     .allowed_methods(vec!["GET"])
                     .allow_any_header(),
             )
-            .service(hello)
+            .service(get_article)
+            .service(upload_article)
+            .service(find_all)
     })
         .workers(num_workers)
         .bind(("127.0.0.1", 8080))?
